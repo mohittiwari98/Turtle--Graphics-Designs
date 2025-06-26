305d9bdee82c0de06f625d7b825d96b922429d0f
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-26 11:12:42 +0530

    New-desigen.py

--- New-desigen.py
+++ New-desigen.py
@@ -5,7 +5,7 @@ t = turtle.Turtle()
 t.speed(0)
 
 colors = ['red,' 'purple,' 'blue,' 'green,' 'orange,' 'yellow']
-#change colors if 
+#change colors if you
 for x in range(360):
     t.pencolor(colors[x % 6])
     t.width(x // 100 + 1)
